29d1f68ca0b509f0716d84c549ff9965134b619d
Agustina Insua <aguss.insua99@gmail.com>
2024-04-25 16:30:45 -0300

    header v2

--- spa-beauty/src/app/components/home/home.component.ts
+++ spa-beauty/src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
+import { ContentComponent } from "../../shared/content/content.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, ContentComponent]
 })
 export class HomeComponent {
 
