6fd893e4a4dd6aba252a74a3877e4d1fcf126eaf
Agustina Insua <aguss.insua99@gmail.com>
2025-07-22 16:59:27 -0300

    add template -example

---
+++ admin-dashboard/src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'] // Make sure this file exists in the same folder as home.component.ts
+})
+export default class HomeComponent {
+
+}
