ac35a162e452a73980d580d06ac5c3a7a25697f9
Agustina Insua <aguss.insua99@gmail.com>
2025-07-25 14:17:44 -0300

    updated about services description

--- admin-dashboard/src/app/components/services/services.component.ts
+++ admin-dashboard/src/app/components/services/services.component.ts
@@ -11,22 +11,22 @@ export class ServicesComponent {
 services = [
     {
       title: 'Massage',
-      description: 'Relax with our expert massage sessions.',
+      description: 'Relax and rejuvenate your body with our professional massage services.',
       image: '/assets/imgs/massages.jpg'
     },
     {
       title: 'Pedicure & Manicure',
-      description: 'Hands & feet pampering with polish & care.',
+      description: 'Indulge in our pedicure and manicure options to flaunt flawless hands and feet.',
       image: '/assets/imgs/manicure.jpg'
     },
     {
       title: 'Definitive Depilation',
-      description: 'Say goodbye to unwanted hair.',
+      description: 'Say goodbye to unwanted hair with our permanent hair removal treatments.',
       image: '/assets/imgs/depilation.jpg'
     },
     {
       title: 'Skin care',
-      description: 'Facials & skincare to enhance your glow.',
+      description: 'Achieve radiant, blemish-free skin with our facial cleansing options.',
       image: '/assets/imgs/skincare.jpg'
     }
   ];
