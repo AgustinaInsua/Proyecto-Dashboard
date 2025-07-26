30080251af1c3eb82a2eb9cee23a6cbd7c2732af
Agustina Insua <aguss.insua99@gmail.com>
2025-07-26 11:32:52 -0300

    add about our products

--- admin-dashboard/src/app/components/home/home.component.ts
+++ admin-dashboard/src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ServicesComponent } from "../services/services.component";
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from "../../shared/header/header.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { AboutProductsComponent } from "../about-products/about-products.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ServicesComponent, HeaderComponent],
+  imports: [ServicesComponent, HeaderComponent, FooterComponent, AboutProductsComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'] // Make sure this file exists in the same folder as home.component.ts
+  styleUrls: ['./home.component.css'] 
 })
 export default class HomeComponent {
 
